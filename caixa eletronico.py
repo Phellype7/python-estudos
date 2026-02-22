dd37f09f5a91267d7a093a212149d7a2d0d2dc60
Luyz <luyzph007@gmail.com>
2026-02-22 17:16:06 -0300

    Update caixa eletronico.py

--- caixa eletronico.py
+++ caixa eletronico.py
@@ -1,2 +1,25 @@
 print('BANCO ITAÚ')
-sa = float(input('Que valor você quer sacar? R$'))
+sa = int(input('Que valor você quer sacar? R$'))
+total = sa
+ced = 100
+toced = 0
+while True:
+    if total >= ced:
+        total -= ced
+        toced += 1
+    else:
+        if toced > 0:
+            print(f'Deu o total {toced} cédulas de R$ {ced}')
+        if ced == 100:
+            ced=50
+        elif ced == 50:
+            ced=20
+        elif ced == 20:
+            ced=10
+        elif ced == 10:
+            ced=5
+        elif ced == 5:
+            ced=1
+        toced = 0
+        if total == 0:
+            break
