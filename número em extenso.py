5f05c537c472f47ee5a0e77aab9dfbb6f9921ba3
Luyz <luyzph007@gmail.com>
2026-04-21 17:33:20 -0300

    new cod and delete cod
    I added the new code (number in words) for the course, and deleted the code for the college assignment.

---
+++ número em extenso.py
@@ -0,0 +1,7 @@
+n = ('zero','um','dois','três','quatro','cinco','seis','sete','oito','nove','dez','onze','doze','treze','quatorze','quinze','dezesseis','dezessete','dezoito','dezenove','vinte')
+while True:
+    n1 = int(input('Digite um número entre 0 e 20: '))
+    if 0<=n1<=20:
+        break
+    print('Número invalido')
+print(f'Você digitou o número {n[n1]}')
